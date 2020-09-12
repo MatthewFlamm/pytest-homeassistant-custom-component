eaf12c04cc868b3760e2ebb8e71c5d18daea532c
Flamm, Matthew H <matthewhflamm@gmail.com>
2020-09-12 12:40:24 +0100

    v0.0.7

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("requirements_test.txt","r") as f:
 setup(
     author="Matthew Flamm",
     name="pytest-homeassistant-custom-component",
-    version="0.0.6",
+    version="0.0.7",
     packages=find_packages(),
     python_requires=">=3.7.1",
     install_requires=requirements,

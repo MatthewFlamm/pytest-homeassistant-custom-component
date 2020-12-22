04691578224dc3ab9956fbf70f7ce35aea6698c6
MatthewFlamm <MatthewFlamm@users.noreply.github.com>
2020-12-22 07:25:52 +0000

    [create-pull-request] automated change

--- pytest_homeassistant_custom_component/const.py
+++ pytest_homeassistant_custom_component/const.py
@@ -3,8 +3,8 @@ Constants used by Home Assistant components.
 
 This file is originally from homeassistant/core and modified by pytest-homeassistant-custom-component.
 """
-MAJOR_VERSION = 0
-MINOR_VERSION = 119
+MAJOR_VERSION = 2021
+MINOR_VERSION = 1
 PATCH_VERSION = "0.dev0"
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"

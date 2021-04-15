876d77a5d3546064da308887c21f966e5105cee7
MatthewFlamm <MatthewFlamm@users.noreply.github.com>
2021-04-15 07:05:18 +0000

    [create-pull-request] automated change

--- pytest_homeassistant_custom_component/const.py
+++ pytest_homeassistant_custom_component/const.py
@@ -5,6 +5,6 @@ This file is originally from homeassistant/core and modified by pytest-homeassis
 """
 MAJOR_VERSION = 2021
 MINOR_VERSION = 4
-PATCH_VERSION = "0b4"
+PATCH_VERSION = "4"
 __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"

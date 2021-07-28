0091dd8ba7a753a77d0196158e052f52ed0349b9
MatthewFlamm <MatthewFlamm@users.noreply.github.com>
2021-07-28 22:40:26 +0000

    [create-pull-request] automated change

--- pytest_homeassistant_custom_component/const.py
+++ pytest_homeassistant_custom_component/const.py
@@ -8,7 +8,7 @@ from __future__ import annotations
 from typing import Final
 
 MAJOR_VERSION: Final = 2021
-MINOR_VERSION: Final = 7
+MINOR_VERSION: Final = 8
 PATCH_VERSION: Final = "0b0"
 __short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__: Final = f"{__short_version__}.{PATCH_VERSION}"

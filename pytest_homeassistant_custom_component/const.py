968577f956e3b1f4ed10681226b2674e1a480b08
Matthew Flamm <MatthewFlamm@users.noreply.github.com>
2021-11-20 05:09:22 +0000

    Bump version

--- pytest_homeassistant_custom_component/const.py
+++ pytest_homeassistant_custom_component/const.py
@@ -9,6 +9,6 @@ from typing import Final
 
 MAJOR_VERSION: Final = 2021
 MINOR_VERSION: Final = 11
-PATCH_VERSION: Final = "4"
+PATCH_VERSION: Final = "5"
 __short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__: Final = f"{__short_version__}.{PATCH_VERSION}"

cf6966147f80942410dccf3c1726ad2e45de9d7a
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2023-01-29 15:47:35 -0500

    [create-pull-request] automated change (#135)
    Co-authored-by: MatthewFlamm <MatthewFlamm@users.noreply.github.com>

--- pytest_homeassistant_custom_component/const.py
+++ pytest_homeassistant_custom_component/const.py
@@ -10,8 +10,8 @@ from typing import Final
 
 APPLICATION_NAME: Final = "HomeAssistant"
 MAJOR_VERSION: Final = 2023
-MINOR_VERSION: Final = 1
-PATCH_VERSION: Final = "7"
+MINOR_VERSION: Final = 2
+PATCH_VERSION: Final = "0b4"
 __short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__: Final = f"{__short_version__}.{PATCH_VERSION}"
-REQUIRED_PYTHON_VER: Final[tuple[int, int, int]] = (3, 9, 0)
+REQUIRED_PYTHON_VER: Final[tuple[int, int, int]] = (3, 10, 0)

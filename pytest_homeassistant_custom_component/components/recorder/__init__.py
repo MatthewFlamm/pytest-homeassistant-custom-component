6921dc8e6b07d8c009948b2c003ca6fe0039116d
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2023-02-25 08:30:35 -0500

    [create-pull-request] automated change (#140)
    Co-authored-by: MatthewFlamm <MatthewFlamm@users.noreply.github.com>

--- pytest_homeassistant_custom_component/components/recorder/__init__.py
+++ pytest_homeassistant_custom_component/components/recorder/__init__.py
@@ -3,3 +3,7 @@ Tests for Recorder component.
 
 This file is originally from homeassistant/core and modified by pytest-homeassistant-custom-component.
 """
+
+import pytest
+
+pytest.register_assert_rewrite("tests.components.recorder.common")

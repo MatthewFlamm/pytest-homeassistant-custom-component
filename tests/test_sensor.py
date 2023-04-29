0080d910a65ef0cfe466bba7f38801780fb317d0
MatthewFlamm <39341281+MatthewFlamm@users.noreply.github.com>
2023-04-29 12:58:39 -0400

    Try multiple tests

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -15,3 +15,16 @@ async def test_sensor(hass):
 
     assert state
     assert state.state == "23"
+
+
+async def test_sensor_2(hass):
+    """Test sensor."""
+    entry = MockConfigEntry(domain=DOMAIN, data={"name": "simple config",})
+    entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    state = hass.states.get("sensor.example_temperature")
+
+    assert state
+    assert state.state == "23"

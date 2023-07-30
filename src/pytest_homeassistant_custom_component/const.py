f394aae14bf95f4a9ab9833c252bac43b266a4bc
Matthew Flamm <MatthewFlamm@users.noreply.github.com>
2023-07-30 05:10:27 +0000

    Bump version

--- src/pytest_homeassistant_custom_component/const.py
+++ src/pytest_homeassistant_custom_component/const.py
@@ -5,13 +5,14 @@ This file is originally from homeassistant/core and modified by pytest-homeassis
 """
 from __future__ import annotations
 
+from enum import StrEnum
 from typing import Final
-
 
 APPLICATION_NAME: Final = "HomeAssistant"
 MAJOR_VERSION: Final = 2023
-MINOR_VERSION: Final = 7
-PATCH_VERSION: Final = "3"
+MINOR_VERSION: Final = 8
+PATCH_VERSION: Final = "0b1"
 __short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__: Final = f"{__short_version__}.{PATCH_VERSION}"
-REQUIRED_PYTHON_VER: Final[tuple[int, int, int]] = (3, 10, 0)
+REQUIRED_PYTHON_VER: Final[tuple[int, int, int]] = (3, 11, 0)
+REQUIRED_NEXT_PYTHON_VER: Final[tuple[int, int, int]] = (3, 11, 0)

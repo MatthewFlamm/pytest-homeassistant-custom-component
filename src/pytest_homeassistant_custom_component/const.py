f223b32d6ec2b2b3b0c86da76d1a2ba7edd0580b
Matthew Flamm <MatthewFlamm@users.noreply.github.com>
2024-01-06 20:07:45 +0000

    Bump version

--- src/pytest_homeassistant_custom_component/const.py
+++ src/pytest_homeassistant_custom_component/const.py
@@ -3,16 +3,9 @@ Constants used by Home Assistant components.
 
 This file is originally from homeassistant/core and modified by pytest-homeassistant-custom-component.
 """
-from __future__ import annotations
-
-from enum import StrEnum
-from typing import Any, Final
-
-APPLICATION_NAME: Final = "HomeAssistant"
+from typing import Final
 MAJOR_VERSION: Final = 2024
 MINOR_VERSION: Final = 1
-PATCH_VERSION: Final = "0"
+PATCH_VERSION: Final = "2"
 __short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__: Final = f"{__short_version__}.{PATCH_VERSION}"
-REQUIRED_PYTHON_VER: Final[tuple[int, int, int]] = (3, 11, 0)
-REQUIRED_NEXT_PYTHON_VER: Final[tuple[int, int, int]] = (3, 11, 0)

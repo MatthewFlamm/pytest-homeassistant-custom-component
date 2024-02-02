9c0945f02bdfad88ad26dda5655ba1c0c0a05e16
Matthew Flamm <MatthewFlamm@users.noreply.github.com>
2024-02-02 05:05:02 +0000

    Bump version

--- src/pytest_homeassistant_custom_component/const.py
+++ src/pytest_homeassistant_custom_component/const.py
@@ -6,6 +6,6 @@ This file is originally from homeassistant/core and modified by pytest-homeassis
 from typing import Final
 MAJOR_VERSION: Final = 2024
 MINOR_VERSION: Final = 2
-PATCH_VERSION: Final = "0b0"
+PATCH_VERSION: Final = "0b2"
 __short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__: Final = f"{__short_version__}.{PATCH_VERSION}"

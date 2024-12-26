76cce78eae03756dce01ec0af40560d074b1b914
Matthew Flamm <MatthewFlamm@users.noreply.github.com>
2024-12-26 05:06:42 +0000

    Bump version

--- src/pytest_homeassistant_custom_component/const.py
+++ src/pytest_homeassistant_custom_component/const.py
@@ -4,8 +4,8 @@ Constants used by Home Assistant components.
 This file is originally from homeassistant/core and modified by pytest-homeassistant-custom-component.
 """
 from typing import TYPE_CHECKING, Final
-MAJOR_VERSION: Final = 2024
-MINOR_VERSION: Final = 12
-PATCH_VERSION: Final = "5"
+MAJOR_VERSION: Final = 2025
+MINOR_VERSION: Final = 1
+PATCH_VERSION: Final = "0b2"
 __short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__: Final = f"{__short_version__}.{PATCH_VERSION}"

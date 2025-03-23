e4527ce42a7460a24674c68c3ede934780e03161
Arie Catsman <120491684+catsmanac@users.noreply.github.com>
2025-03-23 01:42:31 +0100

    Switch to async_forward_entry_setups and async_unload_platforms (#214)

--- custom_components/simple_integration/__init__.py
+++ custom_components/simple_integration/__init__.py
@@ -1,12 +1,8 @@
 """The Simple Integration integration."""
-import asyncio
-
-import voluptuous as vol
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
 
 PLATFORMS = ["sensor"]
 
@@ -19,25 +15,14 @@ async def async_setup(hass: HomeAssistant, config: dict):
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Simple Integration from a config entry."""
 
-    for component in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, component)
-        )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
+
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
     return unload_ok

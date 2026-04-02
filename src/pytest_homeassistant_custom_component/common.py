1e55c90c0160d3e0a1c4ae748de1e2c6c6bd7807
Matthew Flamm <MatthewFlamm@users.noreply.github.com>
2026-04-02 19:24:47 +0000

    Bump version

--- src/pytest_homeassistant_custom_component/common.py
+++ src/pytest_homeassistant_custom_component/common.py
@@ -310,6 +310,8 @@ async def async_test_home_assistant(
         hass
     )
     if load_registries:
+        dr.async_setup(hass)
+
         with (
             patch.object(StoreWithoutWriteLoad, "async_load", return_value=None),
             patch(
--- src/pytest_homeassistant_custom_component/components/diagnostics/__init__.py
+++ src/pytest_homeassistant_custom_component/components/diagnostics/__init__.py
@@ -7,6 +7,7 @@ This file is originally from homeassistant/core and modified by pytest-homeassis
 from http import HTTPStatus
 from typing import cast
 
+from homeassistant.components.diagnostics import DOMAIN
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.device_registry import DeviceEntry
@@ -22,7 +23,7 @@ async def _get_diagnostics_for_config_entry(
     config_entry: ConfigEntry,
 ) -> JsonObjectType:
     """Return the diagnostics config entry for the specified domain."""
-    assert await async_setup_component(hass, "diagnostics", {})
+    assert await async_setup_component(hass, DOMAIN, {})
     await hass.async_block_till_done()
 
     client = await hass_client()
@@ -50,7 +51,7 @@ async def _get_diagnostics_for_device(
     device: DeviceEntry,
 ) -> JsonObjectType:
     """Return the diagnostics for the specified device."""
-    assert await async_setup_component(hass, "diagnostics", {})
+    assert await async_setup_component(hass, DOMAIN, {})
 
     client = await hass_client()
     response = await client.get(
